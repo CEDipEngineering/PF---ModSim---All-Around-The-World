8336ac8a9484be9ae01e02b3a62f220ac447065c
Carlos Dip <cadudip@hotmail.com>
2019-05-23 12:47:40 -0300

    Rewrote EDO.

--- Main.py
+++ Main.py
@@ -8,23 +8,57 @@ M_Terra = 5.972e24
 G = 6.674e-11
 Cd = 1.5
 ro = 1
+m = 1
+A = 0.01
+r_ar = Cd * A/2
+print(G)
+print(M_Terra)
 
 ##------------Função que calcula derivadas-----------#
-def Derivada(lista, t, Ar = True):
+
+x0 = R_Terra + 1
+y0 = 0
+V0 = 1000
+Angulo = 45
+Vx0 = V0 * np.sin(Angulo)
+Vy0 = V0 * np.cos(Angulo)
+
+def EqDif(lista,t):
     X = lista[0]
     Y = lista[1]
     Vx = lista[2]
-    Vy = lista[3]        
-    V = (Vx**2 + Vy**2)**(1/2)          
-    if V != 0 and Ar:
-        cosV = - (Vy/V)                  
-        senV = Vx/V
-    else: 
-        cosV = 0
-        senV = 0
+    Vy = lista[3]  
 
+    print(X)
+    print(Y)
+    print(Vx)
+    print(Vy)
+    #print(dX)
+    #print(dY)
+    #print(D)
+    #print(P)
+    
+     
     dX = Vx
     dY = Vy
-    dVx = (1/m) * (r_ar * V * senV)
-    dVy = g + (1/m) * (r_ar * V * cosV)
-    return dX, dY, dVx, dVy
+    D = (X**2+Y**2)**0.5
+    P = G*M_Terra*m/D**2
+    V = (Vy**2 + Vx**2)**0.5
+    R_ar = 1 * r_ar * V**2
+    senP = Y / D
+    cosP = X / D
+    senAr = Vy / V
+    cosAr = Vx / V
+    dVx = -P*senP -R_ar*senAr
+    dVy = -P*cosP + R_ar*cosAr
+    
+    
+    
+    
+    return dX, dVx, dY, dVy
+
+Timerange = np.arange(0,0.1,0.01)
+
+CI = [x0, Vx0, y0, Vy0] 
+solved = odeint(EqDif, CI, Timerange)
+plt.plot(solved[:,0],solved[:,1], 'ro')
