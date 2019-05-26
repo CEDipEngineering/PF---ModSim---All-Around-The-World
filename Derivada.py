155e58cb1613efd8f7488c67de7bc6a27008da41
CEDipEngineering <cadudip@hotmail.com>
2019-05-26 09:53:03 -0300

    Improved debugging. Remodeled EDOs. Removed air resistance.

--- Derivada.py
+++ Derivada.py
@@ -11,24 +11,18 @@ def Derivada(lista, t, G, M_Terra, m, ro, A, Cd):
     Vx = lista[2]
     Vy = lista[3]  
 
-    print(X)
-    print(Y)
-    print(Vx)
-    print(Vy)
-    
      
+    
+    D = (X**2+Y**2)**0.5
+#    V = (Vy**2 + Vx**2)**0.5
+#    R_ar = (ro*A*Cd*V**2)/2
+#    R_ar = 0
+#    senAr = Vy / V
+#    cosAr = Vx / V
     dX = Vx
     dY = Vy
-    D = (X**2+Y**2)**0.5
-    P = G*M_Terra*m/D**2
-    V = (Vy**2 + Vx**2)**0.5
-    R_ar = (ro*A*Cd*V**2)/2
-    senP = Y / D
-    cosP = X / D
-    senAr = Vy / V
-    cosAr = Vx / V
-    dVx = -P*senP -R_ar*senAr
-    dVy = -P*cosP + R_ar*cosAr
+    dVx = -G*M_Terra*X/(D**(3/2)) # -R_ar*senAr
+    dVy = -G*M_Terra*Y/(D**(3/2)) # + R_ar*cosAr
     
     
     
--- Main.py
+++ Main.py
@@ -1,30 +1,54 @@
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from Derivada import Derivada
+
+
 ##------------Definindo Variáveis-----------#
-R_Terra = 6371000
+R_terra = 6371000
 M_Terra = 5.972e24
-G = 6.674e-11
+G = np.float64(0.0000000000667408)
 Cd = 1.5
 ro = 1
 m = 1
 A = 0.01
-r_ar = Cd * A/2
-print(G)
-print(M_Terra)
 
+#-------------Redefinindo unidades para variáveis------#
+
+
+M_Terra = 6             #Unidade em Yg ou Yottagramas (--> (10**24)g ou (10**21)Kg)
+R_terra = 6.371         #Unidade em Gm ou Megametro (--> (10**6)m)
+print(G)
 ##------------Função que calcula derivadas-----------#
 
-x0 = R_Terra + 1
+x0 = 12
 y0 = 0
-V0 = 1000
-Angulo = 45
-Vx0 = V0 * np.sin(Angulo)
-Vy0 = V0 * np.cos(Angulo)
+#V0 = 100
+#Angulo = 0
+#Vx0 = V0 * np.sin(Angulo)
+#Vy0 = V0 * np.cos(Angulo)
+Vx0 = 100
+Vy0 = 0
 
-
-Timerange = np.arange(0,0.1,0.01)
+Timerange = np.arange(0,100,0.1)
 CI = [x0, Vx0, y0, Vy0] 
 solved = odeint(Derivada, CI, Timerange, args = (G, M_Terra, m, ro, A, Cd,))
-plt.plot(solved[:,0],solved[:,1], 'ro')
+plt.axis([-R_terra*1.1,R_terra*1.1,-R_terra*1.1,R_terra*1.1])
+plt.plot(solved[:,0],solved[:,2])
+plt.title('X por Y')
+plt.grid(True)
+plt.show()
+plt.plot(solved[:,1],solved[:,3])
+plt.title('Vx por Vy')
+plt.grid(True)
+plt.show()
+
+nomes = ['X no tempo', 'Vx no tempo', 'Y no tempo', 'Vy no tempo']
+for i in range(len(nomes)):
+    plt.title(nomes[i])
+    plt.plot(Timerange,solved[:,i])
+    plt.grid(True)
+    plt.show()
+
