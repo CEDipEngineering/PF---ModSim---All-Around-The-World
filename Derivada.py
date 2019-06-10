cca9ace76d7e01e649f7594fc85795099df06dfc
Carlos Dip <cadudip@hotmail.com>
2019-06-10 10:08:47 -0300

    Fixed major bugs. Now producing proper graphs most of the time. Each run yields different results.

--- Derivada.py
+++ Derivada.py
@@ -14,12 +14,14 @@ def Derivada(lista, t, r_ar_d, r_ar_m, w, g, m, I, k_w = 0.07):
     w = lista [4]
     
     V = (vx**2 + vy**2) ** 0.5
-    sena= -vy
-    cosa = vx
+    sena= -vy  # Seria -Vy/V, mas o V foi dividido nas linhas onde o sen e cos aparecem, 
+    cosa = vx  # com intuito de evitar divisões por zero.
     
-    dwdt = -(k_w * w ** 2)/I
-    magx = (r_ar_m * V ** 2 * w * sena)    
-    magy = (r_ar_m * V ** 2 * w * cosa)
+#    Fm = r_ar_m * w * V
+    
+    dwdt = -(k_w * w)
+    magx = (r_ar_m * w * sena)  # Haveria um fator V, contudo, sen seria dividido por V,
+    magy = (r_ar_m * w * cosa)  # então simplificou-se como constante*w*sen/cos
         
 
     dxdt = vx
@@ -29,9 +31,9 @@ def Derivada(lista, t, r_ar_d, r_ar_m, w, g, m, I, k_w = 0.07):
     dvxdt /= m
     dvydt /= m
     if y <= 0:
-        dxdt *= 0.1
-        dydt *= 0.1
-        dvxdt *= 0.1
-        dvydt *= 0.1
+        dxdt *= 0
+        dydt *= 0
+        dvxdt *= 0
+        dvydt *= 0
 
     return dxdt, dydt, dvxdt, dvydt, dwdt
--- Fun.py
+++ Fun.py
@@ -40,24 +40,21 @@ def WporR(lowbnd, upbnd, step):
     
     k_ar = 0.089 
     
-    
-    lista_reach = []
-    lista_w = []
-    for w in np.arange(lowbnd, upbnd, step):
-        CI = [x0, 1000, vx0, vy0, w]
-        solved = scy.odeint(Derivada, CI, lista_tempo, args = (r_ar_d, r_ar_m, w, g, m, I, k_ar,))
-        lista_reach.append(solved[:,0][-1])
-        lista_w.append(w)
-#        plt.plot(solved[:,0], solved[:,1])
-#        plt.title('varying w, current = {0}'.format(w))
-#        plt.show()
-    
-    plt.show()
-    plt.plot(lista_w, lista_reach)
+    for h in range(1000,2001,100):
+        lista_reach = []
+        lista_w = []
+        for w in np.arange(lowbnd, upbnd, step):
+            CI = [x0, h, vx0, vy0, w]
+            solved = scy.odeint(Derivada, CI, lista_tempo, args = (r_ar_d, r_ar_m, w, g, m, I, k_ar,))
+            lista_reach.append(solved[:,0][-1])
+            lista_w.append(w)
+
         
-    #plt.title('Alcances')
+        plt.plot(lista_w, lista_reach, label = 'h = {0}m'.format(h))
+    plt.title('Alcances')
     plt.xlabel('Velocidade angular (rad/s)')
     plt.ylabel('Alcance (m)')
+    plt.legend()
     plt.grid(True)
     plt.xlim((lowbnd + step*2,upbnd))
     plt.savefig('graphwreach.pdf')
@@ -84,7 +81,7 @@ def HporR(lowbnd, upbnd, step):
         
         
     x0 = 0    #m
-    y0 = 0       #m                                                                                                                   
+    y0 = 0       #m  --;;-- É redefinido depois.                                                                                                                 
     vx0 = 0
     vy0 = 0
     w = 150    #rad/s    
@@ -110,9 +107,46 @@ def HporR(lowbnd, upbnd, step):
     plt.savefig('graphhreach.pdf')
     plt.show()  
     
+    
+def trajetoria_w():
+    
+    r = 8.3e-2                
+    Cm = 0.23  #Adimensiona
+    ro = 1.23     #Kg/m3
+    A = (r**2)*3.14   #m2
+    Cd = 0.47   #Adimensional
+    g = 9.81   #m/s2
+    m = 20   #Kg
+    I = 2/3 * m * r ** 2 
+    
+    r_ar_d = ro*A*Cd/2
+    r_ar_m = ro*A*Cm/2
+    
+    tmax = 120
+    deltat = 1e-1
+    lista_tempo = np.arange(0, tmax, deltat)
+        
+        
+    x0 = 0    #m
+    y0 = 500       #m  --;;-- É redefinido depois.                                                                                                                 
+    vx0 = 0
+    vy0 = 0
+    for w in range(50,501,50):    
+        k_ar = 0.12 
+        CI = [x0, y0, vx0, vy0, w]    
+        solved = scy.odeint(Derivada, CI, lista_tempo, args = (r_ar_d, r_ar_m, w, g, m, I, k_ar,))
+        plt.plot(solved[:,0],solved[:,1],label = 'w = {0}rad/s'.format(w))    
+    plt.title('Trajetória')
+    plt.xlabel('Posição em x (m)')
+    plt.ylabel('Posição em y (m)')
+    plt.grid(True)
+    plt.legend()
+    plt.savefig('graphtraject.pdf')
+    plt.show()      
 
-WporR(100,1000,1)
+WporR(100,200,1)
 
 HporR(200,2000,5)
 
+trajetoria_w()
 
